766b9ca977961cde745b5f38bce5bcd0aacda5cd
Eugene Rozhkov <NodCaleb@Teiwaz.local>
2015-03-08 21:39:06 +0200

    Initial Commit

---
+++ TrafficLawsKazakhstan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrafficLawsKazakhstan
+//
+//  Created by Eugene Rozhkov on 08.03.15.
+//  Copyright (c) 2015 Nord Point. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
